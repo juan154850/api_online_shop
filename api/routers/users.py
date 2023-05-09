ed3844a62e518f94b905a181b658c844e4be1dda
Juan bermudez <juan154850@gmail.com>
2023-05-09 16:09:45 -0500

    endpoint for auth

--- api/routers/users.py
+++ api/routers/users.py
@@ -13,8 +13,6 @@ from typing import List
 from passlib.context import CryptContext
 from datetime import timedelta, datetime
 
-
-
 from bson import ObjectId
 
 import os
@@ -22,19 +20,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# from fastapi import APIRouter, status, HTTPException, Depends, Form
-# # from db.models.auth_model import User, UserDb
-# # from db.database import db_client
-# # from functions.auth_functions import search_user_db, current_user
-
-# from bson import ObjectId
-# from pymongo import ReturnDocument
-# # from functions.vip_functions import is_admin
-# from typing import Annotated
-
 
 users_router = APIRouter(prefix="/users", tags=["Users"])
-
 
 
 URI = os.getenv("URI")
@@ -47,24 +34,13 @@ db_client = DataBase(URI, ServerApi("1"))
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 crypt = CryptContext(schemes=["bcrypt"])
-# crypt.
-
-# #This method show the data.
-
-token = Token
-test_create_access_token = token.create_access_token(data={"sub":"backend@onlineshop.com"}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
-print(test_create_access_token)
-test_verify_password = token.verify_password("123456","$2b$12$ZLKSnTjGmdNdseQerw.Ou.vInH5PY2WRXInQuG3zJBRzWJyJabcSS")
-print(test_verify_password)
-test_authenticate_user = token.authenticate_user(token, db_client, "juan.bermudez@onlineshop.com", "123456")
-print(test_authenticate_user)
 
 
 @users_router.get("", response_class=JSONResponse, response_model=User)
-async def get_users() -> List[User]:    
+async def get_users() -> List[User]:
 
     all_users = User.users_schema(
-        self=User, users_list=db_client.db_client.db_users.users.find())      
+        self=User, users_list=db_client.db_client.db_users.users.find())
     return JSONResponse(content=all_users, status_code=200)
 
 
@@ -72,10 +48,11 @@ async def get_users() -> List[User]:
 async def get_users(key: str, value: str):
     try:
         if key == "id":
-            user = db_client.db_client.db_users.users.find_one({"_id": ObjectId(value)})            
-            if(user):                
-                user = User.user_schema(user=user)                
-            if (len(user) > 0):                                
+            user = db_client.db_client.db_users.users.find_one(
+                {"_id": ObjectId(value)})
+            if (user):
+                user = User.user_schema(user=user)
+            if (len(user) > 0):
                 return JSONResponse(
                     content=User.users_schema(
                         self=User, users_list=db_client.db_client.db_users.users.find({"_id": ObjectId(value)})),
@@ -92,12 +69,13 @@ async def get_users(key: str, value: str):
 
 
 @users_router.post("/", response_class=JSONResponse, response_model=UserDb)
-async def create_account(account: UserDb) -> UserDb:    
+async def create_account(account: UserDb) -> UserDb:
     all_users = User.users_schema(
         self=User, users_list=db_client.db_client.db_users.users.find())
 
     if (len(all_users) >= 50):
-        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail={"Message":"Database full."})
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail={
+                            "Message": "Database full."})
 
     if (not (db_client.db_client.db_users.users.find_one(filter={"email": account.email}))):
         if (not (db_client.db_client.db_users.users.find_one(filter={"cellphone": account.cellphone}))):
@@ -108,7 +86,8 @@ async def create_account(account: UserDb) -> UserDb:
                 new_account["role"] = "user"
             new_account["password"] = crypt.hash(secret=SECRET_KEY)
             del new_account["id"]
-            id = db_client.db_client.db_users.users.insert_one(new_account).inserted_id
+            id = db_client.db_client.db_users.users.insert_one(
+                new_account).inserted_id
             new_account = UserDb.user_schema(
                 db_client.db_client.db_users.users.find_one(filter={"_id": id}))
             return JSONResponse(dict(new_account), status_code=201)
@@ -117,50 +96,31 @@ async def create_account(account: UserDb) -> UserDb:
 
     else:
         raise HTTPException(404, "This email already exists.")
-# Original
-# @users_router.put("/{account_id}", response_class=JSONResponse, response_model=UserDb)
-# async def update_user(account_id: str, new_user: dict) -> UserDb:        
-#     try:
-#         # Validate the item...        
-#         if(type(db_client.db_users.users.find_one({"_id": ObjectId(account_id)})) != type(None)):                                    
-#             actual_user = UserDb.user_schema(db_client.db_users.users.find_one({"_id":ObjectId(account_id)}))            
-#             for key in new_user:                
-#                 if( not actual_user.get(key)):
-#                     error = {"Message":"Any key is invalid."}
-#                     raise error
-#             # end for
 
-#             updated_user = db_client.db_users.users.find_one_and_update({'_id': ObjectId(account_id)}, {'$set': new_user}, return_document=ReturnDocument.AFTER)            
-#             return JSONResponse(content=UserDb.user_schema(updated_user))
-#         else:
-#             error = {"Message":"This user does not exist."}
-#             raise error
-#     except:
-#         # if we have an error, send the raise.
-#         raise HTTPException(406, detail=error)     
-    
-# Test
+
 @users_router.put("/{account_id}", response_class=JSONResponse, response_model=UserDb)
-async def update_user(account_id: str, new_user: dict) -> UserDb:        
+async def update_user(account_id: str, new_user: dict) -> UserDb:
     try:
-        # Validate the item...        
-        if(type(db_client.db_client.db_users.users.find_one({"_id": ObjectId(account_id)})) != type(None)):                                    
-            actual_user = (db_client.db_client.db_users.users.find_one({"_id":ObjectId(account_id)}))             
-            for key in new_user:                
-                if( not actual_user.get(key)):
-                    error = {"Message":"Any key is invalid."}
+        # Validate the item...
+        if (type(db_client.db_client.db_users.users.find_one({"_id": ObjectId(account_id)})) != type(None)):
+            actual_user = (db_client.db_client.db_users.users.find_one(
+                {"_id": ObjectId(account_id)}))
+            for key in new_user:
+                if (not actual_user.get(key)):
+                    error = {"Message": "Any key is invalid."}
                     raise error
-                if(key == "password"):
+                if (key == "password"):
                     new_user["password"] = crypt.hash(secret=SECRET_KEY)
-            # end for            
-            updated_user = db_client.db_client.db_users.users.find_one_and_update({'_id': ObjectId(account_id)}, {'$set': new_user}, return_document=ReturnDocument.AFTER)            
+            # end for
+            updated_user = db_client.db_client.db_users.users.find_one_and_update(
+                {'_id': ObjectId(account_id)}, {'$set': new_user}, return_document=ReturnDocument.AFTER)
             return JSONResponse(content=UserDb.user_schema(updated_user))
         else:
-            error = {"Message":"This user does not exist."}
+            error = {"Message": "This user does not exist."}
             raise error
     except:
         # if we have an error, send the raise.
-        raise HTTPException(406, detail=error)     
+        raise HTTPException(406, detail=error)
 
 
 @users_router.delete("/{account_id}")
@@ -177,21 +137,3 @@ async def delete_user(account_id: str):
     except:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={
                             "Message": "This user does not exist."})
-
-
-# @auth_router.post("")
-# async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
-#     user = search_user_db("email", username)
-#     if (user):
-#         #verify the password.
-#         user_db = db_client.db_users.users.find_one({"email":user.email})
-#         print(user_db["password"])
-#         print(crypt.verify(secret="123456",hash=password))
-#         #the users is valid and we can generate the token...
-#         return JSONResponse(content="Login successfully")
-#         # else:
-#             #the password is not correct.
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="The password are not correct.")
-#     else:
-#         print("This users does not exists.")
-#     pass
