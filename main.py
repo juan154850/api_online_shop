2bcf33cbc72a8b965ded6710a7917701330af81d
Juan bermudez <juan154850@gmail.com>
2023-05-11 18:29:46 -0500

    main endpoint add.

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from api.routers.token import token_router
 # from api.routers.users import auth_router
 # from routers.vip_products import vip_products_router
 # from routers.auth_forms import auth_router_forms
-# from fastapi.responses import HTMLResponse
-# from fastapi.templating import Jinja2Templates
-# from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 # from api.models.product import Product
 # from db.database import db_client
 # from bson import ObjectId
@@ -19,9 +19,9 @@ from api.routers.token import token_router
 app = FastAPI()
 app.title = "Online Shop Project"
 app.version = "0.0.1"
-# app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="templates")
 # Routers.
 app.include_router(router=product_router)
 app.include_router(router=users_router)
@@ -31,7 +31,8 @@ app.include_router(router=token_router)
 
 
 @app.get("/")
-async def index():
+async def index(request: Request):
     # search in the data base and return the products
-    return {"message": "welcome to the api"}
-
+    template = "index.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context=context)
